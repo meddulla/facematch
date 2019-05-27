5bfe5cf9cc57bee6f795d75df04440b8322aa3b7
Sofia Cardita <sofiacardita@gmail.com>
2019-05-27 08:43:57 +0100

    Add case info fields

--- people/models.py
+++ people/models.py
@@ -15,16 +15,24 @@ CATEGORIES = (
     ('U', 'Unidentified'),
 )
 
-
-class MissingPerson(models.Model):
-    name = models.CharField(max_length=400, default=None, null=True)
+class Person(models.Model):
     code = models.CharField(max_length=400, default=None, null=True, unique=True)
+    photo = models.ImageField(default=None, null=True)
+    # Case info fields
     gender = models.CharField(
         max_length=2,
         choices=GENDERS,
         default='U',
     )
-    photo = models.ImageField(default=None, null=True, storage=MissingStorage())
+    name = models.CharField(max_length=400, default=None, null=True)
+    current_min_age = models.IntegerField(default=None, null=True)
+    current_max_age = models.IntegerField(default=None, null=True)
+    ethnicity = models.CharField(max_length=400, default=None, null=True) # we only use the 1st one
+    last_sighted = models.DateTimeField(default=None, null=True)
+    # control case info fields
+    case_info_fetched = models.BooleanField(default=False)
+    last_fetched = models.DateTimeField(default=None, null=True)
+
 
     def photo_tag(self):
         url = "https://%s/%s" % (MissingStorage.custom_domain, self.photo)
@@ -33,36 +41,27 @@ class MissingPerson(models.Model):
     photo_tag.short_description = 'Image'
     photo_tag.allow_tags = True
 
+    def __str__(self):
+        return self.name or self.code
+
+    class Meta:
+        abstract = True
+
+
+class MissingPerson(Person):
+    photo = models.ImageField(default=None, null=True, storage=MissingStorage())
+
     def delete(self, *args, **kwargs):
         super().delete(*args, **kwargs)
         S3().delete_file(MissingStorage.bucket_name, file_name=self.photo)
 
-    def __str__(self):
-        return self.name or self.code
 
-
-class UnidentifiedPerson(models.Model):
-    code = models.CharField(max_length=400, default=None, null=True, unique=True)
-    gender = models.CharField(
-        max_length=2,
-        choices=GENDERS,
-        default='U',
-    )
+class UnidentifiedPerson(Person):
     photo = models.ImageField(default=None, null=True, storage=UnidentifiedStorage())
-
-    def photo_tag(self):
-        url = "https://%s/%s" % (UnidentifiedStorage.custom_domain, self.photo)
-        return mark_safe('<img src="%s" width="200px" alt="%s"/>' % (url, self.photo))
-
-    photo_tag.short_description = 'Image'
-    photo_tag.allow_tags = True
 
     def delete(self, *args, **kwargs):
         super().delete(*args, **kwargs)
         S3().delete_file(UnidentifiedStorage.bucket_name, file_name=self.photo)
-
-    def __str__(self):
-        return self.code
 
 
 class MissingFace(models.Model):
