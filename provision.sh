1877e8013b642f2712745ceee1377b330ae66d7a
Sofia Cardita <sofiacardita@gmail.com>
2019-06-03 19:29:11 +0100

    Use runserver for now

--- provision.sh
+++ provision.sh
@@ -9,9 +9,11 @@ sudo apt install git
 
 sudo apt install build-essential libssl-dev libffi-dev python3-dev
 sudo apt-get install python3.7
+sudo apt-get install python3.7-dev # for uwsgi
 sudo apt install -y python3-pip
 sudo apt install -y python3.7-venv
 
+su webapper
 cd
 python3.7 -m venv facematch-venv
 source facematch-venv/bin/activate
@@ -22,3 +24,8 @@ git clone git@github.com:meddulla/facematch.git
 
 cd facematch
 pip install -r requirements.txt
+
+
+sudo cp /home/webapper/facematch/facematch.service /etc/systemd/system/
+sudo systemctl --system enable facematch
+sudo systemctl start facematch
